01cf6db435e878fd12bd24a7ff6936288b3dccf9
INTERC3PTI0N <milnedunk@gmail.com>
2026-07-23 01:36:33 +0530

    bases

--- next.config.js
+++ next.config.js
@@ -3,9 +3,15 @@
 // its own inline + webpack scripts. Serving it verbatim avoids React hydrating
 // (and fighting) the canvas the scripts mount. Everything else lives in public/.
 module.exports = {
+  basePath: '/demo3',
   async rewrites() {
     return {
-      beforeFiles: [{ source: '/', destination: '/index.html' }],
+      beforeFiles: [
+        { 
+          source: '/', 
+          destination: '/index.html' 
+        }
+      ],
     };
   },
 };
